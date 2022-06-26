8946a6cd29b9ed3e17bf22c1ba203d4e395c1baf
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-06-26 14:15:28 +0300

    Inititialization and fetching products

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
-
+import React ,{useEffect}from "react";
+import { fetchProducts } from "./redux/reducer/productReducer";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import { useAppDispatch } from "./redux/hooks";
 
 function App() {
+  const dispatch = useAppDispatch()
+  const productList = useSelector((state: RootState) => {
+    return state.productReducer.productList
+
+  })
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, [])
+
   return (
     <div className="App">
+      {
+        productList.map((item)=>
+        <div key={item.id}>
+          <h3>{item.title}</h3>
+
+
+        </div>
+
+        )
+      }
+
     </div>
   );
 }
