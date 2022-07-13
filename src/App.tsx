94ff2e13fded291537853719c20cc13e0ff1a375
Hafiz Javid <91490403+hafizkh@users.noreply.github.com>
2022-07-13 13:17:36 +0300

    Small Changes in files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import NavBar from "./components/NavBar"
+import NavBar from "./components/Navbar"
 import Cart from "./pages/Cart";
 import ProductItem from "./pages/ProductItem";
 import NotFound from "./pages/NotFound";
